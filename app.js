86e139465d2ba4a290176aec8126463c48d379cf
i.kłusek <J6ZkqwVUn>
2022-04-30 22:55:33 +0200

    code prettier

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//USE express-session, passport, passport-local-mongoose library 
+//USE express-session, passport, passport-local-mongoose library
 //Cookies session
 
 //------------------IMPORT--------------
@@ -13,140 +13,129 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-
-
 //------------------SERVER CONFIG--------------
-LAN_PORT=5000;
+LAN_PORT = 5000;
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.static('public'));
 
-app.use(session({                               //Use the session package, init config
-    secret: process.env.SECRET_EXPRESS_SESSION,
-    resave: false,
-    saveUninitialized: false,
-
-}));
+app.use(
+    session({
+        //Use the session package, init config
+        secret: process.env.SECRET_EXPRESS_SESSION,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
 //Initialize passport, Use passport session
-app.use( passport.initialize());                
-app.use( passport.session());                   
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 let port = process.env.PORT; //use the port form heroku website
-if (port == null || port ==''){
+if (port == null || port == '') {
     port = LAN_PORT;
 }
 
 //------------------DATABASE CONFIG-------------
-mongoose.connect(process.env.MONGODB_CONNECT, 
-    {useNewUrlParser: true}   //mute extra  mongodb errors
+mongoose.connect(
+    process.env.MONGODB_CONNECT,
+    { useNewUrlParser: true } //mute extra  mongodb errors
 );
 // mongoose.set('useCreateIndex', true);        //fix for deprecreation
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
 });
 
-userSchema.plugin(passportLocalMongoose);       //Use this to salt and hash password, and save users to mongoDB
+userSchema.plugin(passportLocalMongoose); //Use this to salt and hash password, and save users to mongoDB
 
-const User = new mongoose.model("User", userSchema);
+const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
-
-
-
-
-
-
-
-
 //-----------------Target websites-------------
 //----------GET---------
-app.get("/", function (req, res) {
-    console.log("Connect to website: " + "Home");
-    res.render("home");
+app.get('/', function (req, res) {
+    console.log('Connect to website: ' + 'Home');
+    res.render('home');
 });
 
-app.get("/login", function (req, res) {
-    console.log("Connect to website: " + "Login");
-    res.render("login");
+app.get('/login', function (req, res) {
+    console.log('Connect to website: ' + 'Login');
+    res.render('login');
 });
 
-app.get("/register", function (req, res) {
-    console.log("Connect to website: " + "Register");
-    res.render("register");
+app.get('/register', function (req, res) {
+    console.log('Connect to website: ' + 'Register');
+    res.render('register');
 });
 
-app.get("/secrets", function (req, res) {
-    console.log("Connect to website: " + "Secret");
-    if( req.isAuthenticated()){
-        res.render("secrets");
-    }else{
-        res.redirect("/login");
+app.get('/secrets', function (req, res) {
+    console.log('Connect to website: ' + 'Secret');
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
     }
-
 });
 
-app.get("/logout", function(req,res){
-    console.log("Processing: " + "Logout");
+app.get('/logout', function (req, res) {
+    console.log('Processing: ' + 'Logout');
     //method from passport package
     req.logout();
     res.redirect('/');
-
-})
-
-//----------POST---------
-app.post("/register", function(req, res){
-
-    //method from passport-local-mongoose package
-    User.register({username: req.body.username}, req.body.password, function (err, user) {
-        if (err){
-            console.log(err);
-            res.redirect("/register");
-        }else{
-            passport.authenticate("local")(req, res, function(){
-                res.redirect("/secrets");
-            })
-        }
-    })
-
 });
 
-app.post("/login", function(req, res){
+//----------POST---------
+app.post('/register', function (req, res) {
+    //method from passport-local-mongoose package
+    User.register(
+        { username: req.body.username },
+        req.body.password,
+        function (err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect('/register');
+            } else {
+                passport.authenticate('local')(req, res, function () {
+                    res.redirect('/secrets');
+                });
+            }
+        }
+    );
+});
 
-    const user = new User({ //created from a mongoose model
+app.post('/login', function (req, res) {
+    const user = new User({
+        //created from a mongoose model
         username: req.body.username,
-        password: req.body.password
+        password: req.body.password,
     });
 
     //method from passport package
-    req.login(user, function(err){
-        if (err){
+    req.login(user, function (err) {
+        if (err) {
             console.log(err);
-        }else{
-            passport.authenticate("local")(req, res, function(){
-                res.redirect("/secrets");
-            })
+        } else {
+            passport.authenticate('local')(req, res, function () {
+                res.redirect('/secrets');
+            });
         }
-    })
-
+    });
 });
 
-
-
 //---------LOG SERVER-----------
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('Sever has started successfully on port: ' + port);
-})
+});
--- app_bcrypt.js
+++ app_bcrypt.js
@@ -8,124 +8,107 @@ const ejs = require('ejs');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const saltRounds=10;
-
+const saltRounds = 10;
 
 //-----------------IMPORT JS FILES-----------
 const secrets = require('./secrets');
 const { StringDecoder } = require('string_decoder');
 const { MongoServerClosedError } = require('mongodb');
 
-
 //------------------SERVER CONFIG--------------
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.static('public'));
-LAN_PORT=5000;
-
-
+LAN_PORT = 5000;
 
 //------------------SERVER CONFIG ---------------------
 let port = process.env.PORT; //use the port form heroku website
-if (port == null || port ==''){
+if (port == null || port == '') {
     port = LAN_PORT;
 }
 
 //------------------DATABASE CONFIG-------------
-mongoose.connect(process.env.MONGODB_CONNECT, 
-    {useNewUrlParser: true}   //mute extra  mongodb errors
+mongoose.connect(
+    process.env.MONGODB_CONNECT,
+    { useNewUrlParser: true } //mute extra  mongodb errors
 );
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
 });
 
-
-
-const User = new mongoose.model("User", userSchema);
-
-
-
-
-
-
-
-
-
-
-
-
+const User = new mongoose.model('User', userSchema);
 
 //-----------------Target websites-------------
 //----------HOME---------
-app.get("/", function (req, res) {
-    console.log("Connect to website: " + "Home");
-    res.render("home");
+app.get('/', function (req, res) {
+    console.log('Connect to website: ' + 'Home');
+    res.render('home');
 });
 
 //----------LOGIN---------
-app.get("/login", function (req, res) {
-    console.log("Connect to website: " + "Login");
-    res.render("login");
+app.get('/login', function (req, res) {
+    console.log('Connect to website: ' + 'Login');
+    res.render('login');
 });
 
 //----------REGISTER---------
-app.get("/register", function (req, res) {
-    console.log("Connect to website: " + "Register");
-    res.render("register");
+app.get('/register', function (req, res) {
+    console.log('Connect to website: ' + 'Register');
+    res.render('register');
 });
 
-
-
-
-app.post("/register", function(req, res){
-    
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+app.post('/register', function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
         const newUser = new User({
-            email:  req.body.username,
-            password: hash
+            email: req.body.username,
+            password: hash,
         });
         newUser.save(function (err) {
-            if (err){
+            if (err) {
                 console.log(err);
-            }else{
-                res.render("secrets");
+            } else {
+                res.render('secrets');
             }
-        })
+        });
     });
-
-   
 });
 
-app.post("/login", function(req, res){
+app.post('/login', function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    User.findOne({
-        email:username
-    }, function(err, foundUser){
-        if(err){
-            console.log(err);
-        }else{
-            if (foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if (result == true) {
-                        res.render("secrets");
-                    }
-                });
-                
-                
+    User.findOne(
+        {
+            email: username,
+        },
+        function (err, foundUser) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser) {
+                    bcrypt.compare(
+                        password,
+                        foundUser.password,
+                        function (err, result) {
+                            if (result == true) {
+                                res.render('secrets');
+                            }
+                        }
+                    );
+                }
             }
         }
-    })
-})
-
+    );
+});
 
 //---------LOG SERVER-----------
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('Sever has started successfully on port: ' + port);
-})
+});
